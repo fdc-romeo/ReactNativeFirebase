b1fdf24cc68945178068299b563601c98fbeb3bd
Shaira Mae Romeo <fdc.romeo@gmail.com>
2022-09-26 17:49:03 +0800

    fix

--- src/screens/LoginPage.js
+++ src/screens/LoginPage.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
 import {StyleSheet, View, Text,Image,ActivityIndicator} from 'react-native';
-import { NavigationActions } from "react-navigation";
-import { LoginComponent } from './/LoadingPage';
 
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
 
 const styles = StyleSheet.create({
   MainView: {
@@ -18,20 +18,16 @@ const styles = StyleSheet.create({
   }
 });
 
-function ImportComponent( {navigation}, props ){
-      setTimeout(() => {
-       navigation.navigate('LoginComponent');
-     }, 2500);
+function LoginComponent ({ navigation }) {
+
     return (
         <View style={styles.MainView}>
           <Image
             source={{uri: "https://reactnative.dev/docs/assets/p_cat1.png"}}
             style={{width: 200, height: 200}}
           />
-           <Text>Hello, I am your cat!</Text> 
-           <ActivityIndicator style={styles.Indicator} size="large" color="#0000ff" />
         </View>
     )
 }
 
-export { ImportComponent }
+export { LoginComponent }
