9d8e18829b9fb0e3f06d810b2e1c56b77d4adc8a
Shaira Mae Romeo <fdc.romeo@gmail.com>
2022-09-27 17:55:45 +0800

    Update LoginPage.js

--- src/screens/LoginPage.js
+++ src/screens/LoginPage.js
@@ -1,5 +1,14 @@
 import React, {Component} from 'react';
-import {StyleSheet, View, Text,Image,ActivityIndicator,SafeAreaView} from 'react-native';
+import {
+  StyleSheet, 
+  View, 
+  Text,
+  Image,
+  ActivityIndicator,
+  SafeAreaView,
+  TextInput,
+  Button
+} from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -7,15 +16,44 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
 const styles = StyleSheet.create({
+
   MainView: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
-  Indicator : {
-    marginTop : 10
 
-  }
+  Indicator : {
+    marginTop : 5
+
+  },
+
+  UserName  : {
+    height: 40,
+    width : 200,
+    padding: 10,
+    fontSize: 15,
+  },
+
+  ImageLogo : {
+    width: 200,
+    height: 200
+  },
+
+  Password   : {
+    height: 40,
+    width : 200,
+    padding: 10,
+    fontSize: 15,
+  },
+
+  BtnLogin   : {
+    height: 40,
+    width : 1000,
+    padding: 10,
+    fontSize: 15,
+  },
+
 });
 
 function LoginPage ({ navigation }) {
@@ -23,11 +61,32 @@ function LoginPage ({ navigation }) {
        <SafeAreaView style={{flex: 1}}>
         <View style={styles.MainView}>
           <Image
-            source={{uri: "https://reactnative.dev/docs/assets/p_cat1.png"}}
-            style={{width: 200, height: 200}}
+              source={{uri: "https://reactnative.dev/docs/assets/p_cat1.png"}}
+              style={styles.ImageLogo}
+            />
+            <Text>Please Login</Text>
+
+           <TextInput
+              style={styles.UserName}
+               placeholder="User Name" >
+           </TextInput> 
+
+         <TextInput style={styles.Password}
+              placeholder="Password"
+              placeholderTextColor="#9a73ef"
+              returnKeyType='go'
+              secureTextEntry
+              autoCorrect={false}
           />
-          <Text>Please Login</Text> 
+
+            <Button
+              style={styles.BtnLogin}
+              title="Login"
+              type ="password"
+              onPress={() => alert('Simple Button pressed')}>
+            </Button>
         </View>
+
        </SafeAreaView>  
     )
 }
